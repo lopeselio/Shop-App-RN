4aceb0e8f3556f667e1677cf94080c41f37a8406
lopeselio <lopeselio@gmail.com>
2020-05-16 13:29:01 +0530

    fixed product details

--- shopAPP-RN/navigation/ShopNavigator.js
+++ shopAPP-RN/navigation/ShopNavigator.js
@@ -1,38 +1,23 @@
-// import React from 'react'
-import {
-  createStackNavigator,
-  // createDrawerNavigator,
-  createAppContainer
-} from 'react-navigation'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
 import { Platform } from 'react-native'
-// import { Ionicons } from '@expo/vector-icons'
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
-// import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
-// import CartScreen from '../screens/shop/CartScreen'
-// import OrdersScreen from '../screens/shop/OrdersScreen'
-// import UserProductsScreen from '../screens/user/UserProductsScreen'
-// import EditProductScreen from '../screens/user/EditProductScreen'
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
 import Colors from '../constants/Colors'
-
-const defaultNavOptions = {
-  headerStyle: {
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-  },
-  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-}
 
 const ProductsNavigator = createStackNavigator(
   {
-    ProductsOverview: ProductsOverviewScreen
-    // ProductDetail: ProductDetailScreen,
-    // Cart: CartScreen
+    ProductsOverview: ProductsOverviewScreen,
+    ProductDetail: ProductDetailScreen
   },
   {
-    defaultNavigationOptions: defaultNavOptions
-
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+    }
   }
-
 )
 
 export default createAppContainer(ProductsNavigator)
--- shopAPP-RN/screens/shop/ProductsOverviewScreen.js
+++ shopAPP-RN/screens/shop/ProductsOverviewScreen.js
@@ -17,12 +17,17 @@ const ProductsOverviewScreen = props => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetail={() => {}}
+          onViewDetail={() => {
+            props.navigation.navigate('ProductDetail', { productId: itemData.item.id, ProductTitle: itemData.item.title })
+          }}
           onAddToCart={() => {}}
         />
       )}
     />
   )
+}
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products'
 }
 
 export default ProductsOverviewScreen
