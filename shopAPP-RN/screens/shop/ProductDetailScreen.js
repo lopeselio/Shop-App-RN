b64d6628a8737a93bf8a583c6dc92d557d1f4828
lopeselio <lopeselio@gmail.com>
2020-05-17 17:47:18 +0530

    detail screen

--- shopAPP-RN/screens/shop/ProductDetailScreen.js
+++ shopAPP-RN/screens/shop/ProductDetailScreen.js
@@ -7,21 +7,29 @@ import {
   Button,
   StyleSheet
 } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import Colors from '../../constants/Colors'
+import * as cartActions from '../../store/actions/cart'
 
 const ProductDetailScreen = props => {
   const productId = props.navigation.getParam('productId')
   const selectedProduct = useSelector(state =>
     state.products.availableProducts.find(prod => prod.id === productId)
   )
+  const dispatch = useDispatch()
 
   return (
     <ScrollView>
       <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
       <View style={styles.actions}>
-        <Button color={Colors.primary} title='Add to Cart' onPress={() => {}} />
+        <Button
+          color={Colors.primary}
+          title='Add to Cart'
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct))
+          }}
+        />
       </View>
       <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
       <Text style={styles.description}>{selectedProduct.description}</Text>
@@ -45,11 +53,11 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   price: {
-    fontFamily: 'open-sans-bold',
     fontSize: 20,
     color: '#888',
     textAlign: 'center',
-    marginVertical: 20
+    marginVertical: 20,
+    fontFamily: 'open-sans-bold'
   },
   description: {
     fontFamily: 'open-sans',
