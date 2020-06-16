17683a0535d18eff662a0b77244b337d891853d9
lopeselio <lopeselio@gmail.com>
2020-06-16 23:51:38 +0530

    feat: clearing items from cart LOGIC

--- shopAPP-RN/screens/shop/CartScreen.js
+++ shopAPP-RN/screens/shop/CartScreen.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+
 import Colors from '../../constants/Colors'
 import CartItem from '../../components/shop/CartItem'
+import * as cartActions from '../../store/actions/cart'
 
 const CartScreen = props => {
   const cartTotalAmount = useSelector(state => state.cart.totalAmount)
@@ -17,8 +19,11 @@ const CartScreen = props => {
         sum: state.cart.items[key].sum
       })
     }
-    return transformedCartItems
+    return transformedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1
+    )
   })
+  const dispatch = useDispatch()
 
   return (
     <View style={styles.screen}>
@@ -41,13 +46,15 @@ const CartScreen = props => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => {
+              dispatch(cartActions.removeFromCart(itemData.item.productId))
+            }}
           />
         )}
       />
     </View>
   )
-};
+}
 
 const styles = StyleSheet.create({
   screen: {
