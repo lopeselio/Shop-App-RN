679f64b6dc848b1a8803cc2886d4727520964c51
lopeselio <lopeselio@gmail.com>
2020-06-17 00:14:14 +0530

    redux logic

--- shopAPP-RN/models/order.js
+++ shopAPP-RN/models/order.js
@@ -8,16 +8,16 @@ class Order {
     this.date = date
   }
 
-  get readableDate () {
-    //   return this.date.toLocaleDateString('en-EN', {
-    //       year: 'numeric',
-    //       month: 'long',
-    //       day: 'numeric',
-    //       hour: '2-digit',
-    //       minute: '2-digit'
-    //   });
-    return moment(this.date).format('MMMM Do YYYY, hh:mm')
-  }
-}
+//   get readableDate () {
+//     //   return this.date.toLocaleDateString('en-EN', {
+//     //       year: 'numeric',
+//     //       month: 'long',
+//     //       day: 'numeric',
+//     //       hour: '2-digit',
+//     //       minute: '2-digit'
+//     //   });
+//     return moment(this.date).format('MMMM Do YYYY, hh:mm')
+//   }
+// }
 
 export default Order
