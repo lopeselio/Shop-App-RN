1bb09943c3c7b815cac433742442341f318e8850
lopeselio <lopeselio@gmail.com>
2020-06-19 23:36:27 +0530

    feat: Order Screen

---
+++ shopAPP-RN/screens/shop/OrderScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Flatlist } from 'react-native'
+import { useSelector } from 'react-redux'
+
+const OrdersScreen = props => {
+  const orders = useSelector(state => state.orders.orders)
+  return <Flatlist data={orders} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>} />
+}
+
+export default OrdersScreen
