e334e9715effe61a868a0a033adc8e71cde6798e
lopeselio <lopeselio@gmail.com>
2020-06-19 23:47:24 +0530

    feat: creating Drawer Navigation

--- shopAPP-RN/navigation/ShopNavigator.js
+++ shopAPP-RN/navigation/ShopNavigator.js
@@ -1,11 +1,25 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createStackNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation'
 import { Platform } from 'react-native'
-
+import OrdersScreen from '../screens/shop/OrderScreen'
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
 import CartScreen from '../screens/shop/CartScreen'
 import Colors from '../constants/Colors'
 
+const defaultNavOptions = {
+
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold'
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans'
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
+
+}
 const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverviewScreen,
@@ -13,19 +27,14 @@ const ProductsNavigator = createStackNavigator(
     Cart: CartScreen
   },
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-      },
-      headerTitleStyle: {
-        fontFamily: 'open-sans-bold'
-      },
-      headerBackTitleStyle: {
-        fontFamily: 'open-sans'
-      },
-      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-    }
+    defaultNavigationOptions: defaultNavOptions
   }
 )
+
+const OrdersNavigator = createStackNavigator({
+  orders: OrdersScreen
+}, {
+  defaultNavigationOptions: defaultNavOptions
+})
 
 export default createAppContainer(ProductsNavigator)
