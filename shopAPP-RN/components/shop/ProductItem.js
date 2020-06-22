ae330d8f3c8046ef6f70107ddbc09ec4cf384774
lopeselio <lopeselio@gmail.com>
2020-06-23 02:47:12 +0530

    productItem component, reorganisation and deleting the items

--- shopAPP-RN/components/shop/ProductItem.js
+++ shopAPP-RN/components/shop/ProductItem.js
@@ -4,13 +4,10 @@ import {
   Text,
   Image,
   StyleSheet,
-  Button,
   TouchableOpacity,
   TouchableNativeFeedback,
   Platform
 } from 'react-native'
-
-import Colors from '../../constants/Colors'
 
 const ProductItem = props => {
   let TouchableCmp = TouchableOpacity
@@ -22,7 +19,7 @@ const ProductItem = props => {
   return (
     <View style={styles.product}>
       <View style={styles.touchable}>
-        <TouchableCmp onPress={props.onViewDetail} useForeground>
+        <TouchableCmp onPress={props.onSelect} useForeground>
           <View>
             <View style={styles.imageContainer}>
               <Image style={styles.image} source={{ uri: props.image }} />
@@ -32,16 +29,7 @@ const ProductItem = props => {
               <Text style={styles.price}>${props.price.toFixed(2)}</Text>
             </View>
             <View style={styles.actions}>
-              <Button
-                color={Colors.primary}
-                title='View Details'
-                onPress={props.onViewDetail}
-              />
-              <Button
-                color={Colors.primary}
-                title='To Cart'
-                onPress={props.onAddToCart}
-              />
+              {props.children}
             </View>
           </View>
         </TouchableCmp>
