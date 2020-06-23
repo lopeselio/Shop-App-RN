9af6390c807bf9f208f8b1454d7784f9352f89e5
lopeselio <lopeselio@gmail.com>
2020-06-23 17:26:29 +0530

    created product form

--- shopAPP-RN/screens/user/EditProductsScreen.js
+++ shopAPP-RN/screens/user/EditProductsScreen.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import { View, StyleSheet, ScrollView, Text } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
 
 const EditProductScreen = props => {
   return (
     <ScrollView>
-      <View>
-        <Text>This is the edit product screen</Text>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>ImageURL</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Price</Text>
+          <TextInput style={styles.input} />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput style={styles.input} />
+        </View>
       </View>
     </ScrollView>
   )
