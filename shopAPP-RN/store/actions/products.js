7b83892557e17a995623ed49577e558321e26147
lopeselio <lopeselio@gmail.com>
2020-06-23 18:33:38 +0530

    creating and updating actions dispatched

--- shopAPP-RN/store/actions/products.js
+++ shopAPP-RN/store/actions/products.js
@@ -1,6 +1,6 @@
 export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const CREATE_PRODUCT = 'CREATE_PRODUCT'
-export const UDATE_PRODUCT = 'UPDATE_PRODUCT'
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId }
@@ -17,15 +17,15 @@ export const createProduct = (title, description, imageUrl, price) => {
     }
   }
 }
-export const updateProduct = (id, title, description, imageUrl, price) => {
+
+export const updateProduct = (id, title, description, imageUrl) => {
   return {
-    type: CREATE_PRODUCT,
+    type: UPDATE_PRODUCT,
     pid: id,
     productData: {
       title,
       description,
-      imageUrl,
-      price
+      imageUrl
     }
   }
-}
+};
