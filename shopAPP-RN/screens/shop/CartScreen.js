f09fbde908540e8c9932b5172dbe9cf95f9d495b
lopeselio <lopeselio@gmail.com>
2020-06-23 22:32:38 +0530

    added card in UI

--- shopAPP-RN/screens/shop/CartScreen.js
+++ shopAPP-RN/screens/shop/CartScreen.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import Colors from '../../constants/Colors'
 import CartItem from '../../components/shop/CartItem'
+import Card from '../../components/UI/Card'
 import * as cartActions from '../../store/actions/cart'
 import * as ordersActions from '../../store/actions/orders'
 
@@ -28,10 +29,12 @@ const CartScreen = props => {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:{' '}
-          <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}</Text>
+          <Text style={styles.amount}>
+            ${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}
+          </Text>
         </Text>
         <Button
           color={Colors.accent}
@@ -41,7 +44,7 @@ const CartScreen = props => {
             dispatch(ordersActions.addOrder(cartItems, cartTotalAmount))
           }}
         />
-      </View>
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={item => item.productId}
@@ -74,14 +77,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 20,
-    padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white'
+    padding: 10
   },
   summaryText: {
     fontFamily: 'open-sans-bold',
