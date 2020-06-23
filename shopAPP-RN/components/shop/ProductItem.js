0ba84d3ac1a8930dd9a0fec0d118ab48cc8fcb59
lopeselio <lopeselio@gmail.com>
2020-06-23 16:43:03 +0530

    fixed minus in cart price

--- shopAPP-RN/components/shop/ProductItem.js
+++ shopAPP-RN/components/shop/ProductItem.js
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
   },
   details: {
     alignItems: 'center',
-    height: '15%',
+    height: '17%',
     padding: 10
   },
   title: {
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    height: '25%',
+    height: '23%',
     paddingHorizontal: 20
   }
 })
--- shopAPP-RN/screens/user/UserProductsScreen.js
+++ shopAPP-RN/screens/user/UserProductsScreen.js
@@ -62,6 +62,17 @@ UserProductsScreen.navigationOptions = navData => {
           }}
         />
       </HeaderButtons>
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title='Add'
+          iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+          onPress={() => {
+            navData.navigation.navigate('EditProduct')
+          }}
+        />
+      </HeaderButtons>
     )
   }
 }
