3773b87a98328fe841931b276867cffee649a737
lopeselio <lopeselio@gmail.com>
2020-06-30 18:13:55 +0530

    validation errors fixed

--- shopAPP-RN/components/UI/input.js
+++ shopAPP-RN/components/UI/input.js
@@ -72,7 +72,11 @@ const Input = props => {
         onChangeText={textChangeHandler}
         onBlur={lostFocusHandler}
       />
-      {!inputState.isValid && <Text>{props.errorText}</Text>}
+      {!inputState.isValid && inputState.touched && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{props.errorText}</Text>
+        </View>
+      )}
     </View>
   )
 }
@@ -90,6 +94,14 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     borderBottomColor: '#ccc',
     borderBottomWidth: 1
+  },
+  errorContainer: {
+    marginVertical: 5
+  },
+  errorText: {
+    fontFamily: 'open-sans',
+    color: 'red',
+    fontSize: 13
   }
 })
 
