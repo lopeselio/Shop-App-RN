6dbea3af98edd989eaecbcb7589842a0a37df8e7
lopeselio <lopeselio@gmail.com>
2020-07-02 12:35:05 +0530

    fixed overview screen

--- shopAPP-RN/screens/shop/ProductsOverviewScreen.js
+++ shopAPP-RN/screens/shop/ProductsOverviewScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FlatList, Button, Platform } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
@@ -6,11 +6,16 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../../components/UI/HeaderButton'
 import ProductItem from '../../components/shop/ProductItem'
 import * as cartActions from '../../store/actions/cart'
+import * as productsActions from '../../store/actions/products'
 import Colors from '../../constants/Colors'
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(productsActions.fetchProducts())
+  }, [dispatch])
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate('ProductDetail', {
--- shopAPP-RN/store/actions/products.js
+++ shopAPP-RN/store/actions/products.js
@@ -1,6 +1,38 @@
+import Product from '../../models/product'
+
 export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const CREATE_PRODUCT = 'CREATE_PRODUCT'
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
+export const SET_PRODUCTS = 'SET_PRODUCTS'
+
+export const fetchProducts = () => {
+  const fetch = require('node-fetch')
+  return async dispatch => {
+    // any async code you want!
+    const response = await fetch(
+      'https://rn-complete-guide.firebaseio.com/products.json'
+    )
+
+    const resData = await response.json()
+    const loadedProducts = []
+
+    for (const key in resData) {
+      loadedProducts.push(
+        new Product(
+          key,
+          'u1',
+          resData[key].title,
+          resData[key].imageUrl,
+          resData[key].description,
+          resData[key].price
+        )
+      )
+
+    }
+
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts })
+  }
+}
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId }
@@ -10,18 +42,21 @@ export const createProduct = (title, description, imageUrl, price) => {
   const fetch = require('node-fetch')
   return async dispatch => {
     // any async code you want!
-    const response = await fetch('https://shopapp-rn-f4814.firebaseio.com/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title,
-        description,
-        imageUrl,
-        price
-      })
-    })
+    const response = await fetch(
+      'https://shopapp-rn-f4814.firebaseio.com/products.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price
+        })
+      }
+    )
 
     const resData = await response.json()
 
@@ -48,4 +83,4 @@ export const updateProduct = (id, title, description, imageUrl) => {
       imageUrl
     }
   }
-}
+};
