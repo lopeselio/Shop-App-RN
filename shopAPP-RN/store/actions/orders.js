700b04f88eb44b936d7126b622c24377f33efbfb
lopeselio <lopeselio@gmail.com>
2020-07-05 00:54:38 +0530

    node fetch imports

--- shopAPP-RN/store/actions/orders.js
+++ shopAPP-RN/store/actions/orders.js
@@ -1,8 +1,38 @@
 export const ADD_ORDER = 'ADD_ORDER'
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: { items: cartItems, amount: totalAmount }
+  const fetch = require('node-fetch')
+  return async dispatch => {
+    const date = new Date()
+    const response = await fetch(
+      'https://shopapp-reactnative-e0556.firebaseio.com/orders/u1.json',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          cartItems,
+          totalAmount,
+          date: date.toISOString()
+        })
+      }
+    )
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
+
+    const resData = await response.json()
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date
+      }
+    })
   }
 }
