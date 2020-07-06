d942deffb22316e9726330c6e9a9685357dc5541
lopeselio <lopeselio@gmail.com>
2020-07-07 05:23:12 +0530

    orders addOrder

--- shopAPP-RN/store/actions/orders.js
+++ shopAPP-RN/store/actions/orders.js
@@ -39,10 +39,11 @@ export const fetchOrders = () => {
 export const addOrder = (cartItems, totalAmount) => {
   const fetch = require('node-fetch')
 
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token
     const date = new Date()
     const response = await fetch(
-      'https://shopapp-reactnative-e0556.firebaseio.com/orders/u1.json',
+      `https://shopapp-reactnative-e0556.firebaseio.com/orders/u1.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
