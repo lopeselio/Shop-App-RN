15418c70920be2ed919e57c23b54de1520ce3c6c
lopeselio <lopeselio@gmail.com>
2020-07-07 03:42:32 +0530

    added login auth

--- shopAPP-RN/store/actions/auth.js
+++ shopAPP-RN/store/actions/auth.js
@@ -1,11 +1,10 @@
-// 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDnOdYmXTl9w2QUerDiD8seec0dIDlbJOo',
 export const SIGNUP = 'SIGNUP'
+export const LOGIN = 'LOGIN'
 
 export const signup = (email, password) => {
-  const fetch = require('node-fetch')
   return async dispatch => {
     const response = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDnOdYmXTl9w2QUerDiD8seec0dIDlbJOo',
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDnOdYmXTl9w2QUerDiD8seec0dIDlbJOo',
       {
         method: 'POST',
         headers: {
@@ -26,5 +25,32 @@ export const signup = (email, password) => {
     const resData = await response.json()
     console.log(resData)
     dispatch({ type: SIGNUP })
+  }
+}
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDnOdYmXTl9w2QUerDiD8seec0dIDlbJOo',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    )
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
+
+    const resData = await response.json()
+    console.log(resData)
+    dispatch({ type: LOGIN })
   }
 }
