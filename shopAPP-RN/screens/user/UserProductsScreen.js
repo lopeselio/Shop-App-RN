3dad01ac861b32ea8a75ee0b5e365886c4b935b5
lopeselio <lopeselio@gmail.com>
2020-07-07 06:13:14 +0530

    message to display if there are no products

--- shopAPP-RN/screens/user/UserProductsScreen.js
+++ shopAPP-RN/screens/user/UserProductsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlatList, Button, Platform, Alert } from 'react-native'
+import { FlatList, Button, View, Text, Platform, Alert } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
@@ -27,6 +27,14 @@ const UserProductsScreen = props => {
         }
       }
     ])
+  }
+
+  if (userProducts.length === 0) {
+    return (
+      <View style={{ flex:1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>No products found, start creating some!</Text>
+      </View>
+    )
   }
 
   return (
