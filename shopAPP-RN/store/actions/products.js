156437437a2b13f17b963eb5769620d34f23038e
lopeselio <lopeselio@gmail.com>
2020-07-07 06:23:25 +0530

    added node fetch

--- shopAPP-RN/store/actions/products.js
+++ shopAPP-RN/store/actions/products.js
@@ -1,5 +1,4 @@
 import Product from '../../models/product'
-
 export const DELETE_PRODUCT = 'DELETE_PRODUCT'
 export const CREATE_PRODUCT = 'CREATE_PRODUCT'
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
@@ -7,14 +6,13 @@ export const SET_PRODUCTS = 'SET_PRODUCTS'
 
 export const fetchProducts = () => {
   const fetch = require('node-fetch')
-  return async (dispatch, getState) => {
-    const token = getState().auth.token
-    const userId = getState().auth.userId
 
+  return async (dispatch, getState) => {
     // any async code you want!
+    const userId = getState().auth.userId
     try {
       const response = await fetch(
-        `https://shopapp-reactnative-e0556.firebaseio.com/products.json?auth=${token}`
+        'https://shopapp-reactnative-e0556.firebaseio.com/products.json'
       )
 
       if (!response.ok) {
@@ -28,7 +26,7 @@ export const fetchProducts = () => {
         loadedProducts.push(
           new Product(
             key,
-            'u1',
+            resData[key].ownerId,
             resData[key].title,
             resData[key].imageUrl,
             resData[key].description,
@@ -37,7 +35,11 @@ export const fetchProducts = () => {
         )
       }
 
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts, userProducts.filter(prod => prod.ownerId === userId) })
+      dispatch({
+        type: SET_PRODUCTS,
+        products: loadedProducts,
+        userProducts: loadedProducts.filter(prod => prod.ownerId === userId)
+      })
     } catch (err) {
       // send to custom analytics server
       throw err
@@ -49,10 +51,9 @@ export const deleteProduct = productId => {
   const fetch = require('node-fetch')
 
   return async (dispatch, getState) => {
-    const token = getState().auth.tokem
-
+    const token = getState().auth.token
     const response = await fetch(
-      `https://shopapp-reactnative-e0556.firebaseio.com/products/${productId}.json`,
+      `https://shopapp-reactnative-e0556.firebaseio.com/products/${productId}.json?auth=${token}`,
       {
         method: 'DELETE'
       }
@@ -69,10 +70,9 @@ export const createProduct = (title, description, imageUrl, price) => {
   const fetch = require('node-fetch')
 
   return async (dispatch, getState) => {
+    // any async code you want!
     const token = getState().auth.token
     const userId = getState().auth.userId
-
-    // any async code you want!
     const response = await fetch(
       `https://shopapp-reactnative-e0556.firebaseio.com/products.json?auth=${token}`,
       {
@@ -125,6 +125,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         })
       }
     )
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!')
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
