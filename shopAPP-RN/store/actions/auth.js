b51e94ff89c8eea9acd93b6f0bb68a134b2f8fb4
lopeselio <lopeselio@gmail.com>
2020-07-07 12:43:56 +0530

    created timestamp

--- shopAPP-RN/store/actions/auth.js
+++ shopAPP-RN/store/actions/auth.js
@@ -72,6 +72,7 @@ export const login = (email, password) => {
     const resData = await response.json()
     console.log(resData)
     dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId })
+    const expirationDate = new Date(new Date().getTime + parseInt(resData.expiresIn) * 1000)
     SaveDataToStorage(resData.idToken, resData.localId)
   }
 }
